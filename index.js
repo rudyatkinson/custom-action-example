54c44e2c4b917e61f9f7f0a6e0abe042e3dbabbf
aydnbilal@outlook.com <bilal@arvisgames.com>
2022-12-01 18:57:08 +0300

    *entity token added.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const playFabServer = require("playfab-sdk/Scripts/PlayFab/PlayFabServer");
+const playFabAuthentication = require("playfab-sdk/Scripts/PlayFab/PlayFabAuthentication");
 
 const developerSecretKey = core.getInput('developer-secret-key', {required: true});
 const titleId = core.getInput('title-id', {required: true});
@@ -11,6 +12,10 @@ async function run()
     
     console.log('Given developer secret key: ${developerSecretKey}');
     console.log('Given title id: ${titleId}');
+
+    const entityToken = playFabAuthentication.GetEntityToken;
+    
+    console.log('entity token: ' + entityToken);
 }
 
 run();
