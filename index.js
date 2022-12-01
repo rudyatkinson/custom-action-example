5a7c7ef868f00e823895c430101f229d35f2ad9f
aydnbilal@outlook.com <bilal@arvisgames.com>
2022-12-01 17:50:31 +0300

    fix.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-(
-    async () => {
-        try {
-            core.notice("Calling our action");
-        }catch (error)
-        {
-            core.setFailed(error.message);
-        }
+async () => {
+    try {
+        core.notice("Calling our action");
+    }catch (error)
+    {
+        core.setFailed(error.message);
     }
-)();
+};
