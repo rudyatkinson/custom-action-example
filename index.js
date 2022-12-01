de10e4b9a4f1b927bc0d70bbdac321178c699f44
aydnbilal@outlook.com <bilal@arvisgames.com>
2022-12-01 18:34:00 +0300

    *PlayFab Sdk imported and inputs added to metadata.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
+const playFabServer = require("playfab-sdk/Scripts/PlayFab/PlayFabServer");
 
-const message = core.getInput('message', {required: true});
- 
+const developerSecretKey = core.getInput('DEVELOPER_SECRET_KEY', {required: true});
+const titleId = core.getInput('TITLE_ID', {required: true});
 
 async function run()
 {
-    try {
-        core.notice(message);
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+    playFabServer.settings.developerSecretKey = developerSecretKey;
+    playFabServer.settings.titleId = titleId;
+    
+    core.notice(developerSecretKey);
+    core.notice(titleId);
 }
 
 run();
