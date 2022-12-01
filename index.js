5a852c6812668b4674e37ffcfa652c6730d63891
aydnbilal@outlook.com <bilal@arvisgames.com>
2022-12-01 19:14:54 +0300

    *null check.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const playFabServer = require("playfab-sdk/Scripts/PlayFab/PlayFabServer");
 const playFabAuthentication = require("playfab-sdk/Scripts/PlayFab/PlayFabAuthentication");
+const playFabClient = require("playfab-sdk/Scripts/PlayFab/PlayFabClient");
 
 const developerSecretKey = core.getInput('developer-secret-key', {required: true});
 const titleId = core.getInput('title-id', {required: true});
@@ -13,11 +14,16 @@ async function run()
     console.log('Given developer secret key: ${developerSecretKey}');
     console.log('Given title id: ${titleId}');
 
-    const entityToken = playFabAuthentication.GetEntityToken({callback: response => {
-        console.log('response: ' + response);
-    }});
+    var getEntityTokenRequest = {};
+
+    const entityToken = playFabAuthentication.GetEntityToken(getEntityTokenRequest, GetEntityTokenCallback);
     
-    console.log('entity token: ' + entityToken);
+    console.log('entity token: ' + entityToken == null);
+}
+
+function GetEntityTokenCallback(error, result) {
+    console.log(error);
+    console.log(result);
 }
 
 run();
